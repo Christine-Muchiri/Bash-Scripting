1430cea1a88c0e74c286c99dff0aafed435ded84
Christine-Muchiri <christinemuchiri749@gmail.com>
2023-08-24 22:03:49 +0300

    learning bash

---
+++ OI_redirection/mee.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat << EOF
+first line
+second line
+EOF
+
+wc << EOF
+first line
+second line
+EOF
---
+++ Variables/hellothere.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FIRST_NAME=Christine
+LAST_NAME=Muchiri
+echo Hello $FIRST_NAME $LAST_NAME
